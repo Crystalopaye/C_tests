481b93d6d4dbb759c32b736da0840039c5700e6b
crystalopaye <crystalopaye1@gmail.com>
2023-08-09 15:21:42 +0000

    arithmetic operator file

---
+++ arithmetic_operators/arithmetic_operator_combinations.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - computes the results of arithmetic operations.
+ *
+ * Return: 0
+ */
+
+int main()
+{
+/* declare variables */
+int z, x, y;
+
+/* initialize variables */
+z = 0;
+x = 5;
+y = 10;
+
+/* basic combinations */
+z = x + y;
+printf("z: %d", z);
+return (0);
+}
